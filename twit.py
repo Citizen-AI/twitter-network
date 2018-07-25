0f1956db005558c24f1868c2df2045a56cea64f9
Matthew Bartlett <mhjbnz@gmail.com>
2018-07-26 10:53:32 +1200

    Remove adventitious api call; get mp descriptions

--- twit.py
+++ twit.py
@@ -9,6 +9,21 @@ import unicodecsv as csv
 import re
 import os
 
+def get_party(description):
+    party = "MP"
+    if re.search("labour", description, re.IGNORECASE):
+        party = "Labour"
+    elif re.search("green", description, re.IGNORECASE):
+        party = "Green"
+    elif re.search("national", description, re.IGNORECASE):
+        party = "National"
+    elif re.search("new zealand first|nz first|nz_first", description, re.IGNORECASE):
+        party = "New Zealand First"
+    elif re.search("act", description, re.IGNORECASE):
+        party = "ACT"
+    return party
+
+print "Connecting to Twitter… (pauses to stay under rate limit)"
 api = twitter.Api(consumer_key=os.getenv("consumer_key"),
                   consumer_secret=os.getenv("consumer_secret"),
                   access_token_key=os.getenv("access_token_key"),
@@ -19,22 +34,9 @@ mps = api.GetListMembers(slug="mps", owner_screen_name="NZParliament")
 
 with open('mps.csv', 'wb') as mps_csvfile:
     writer = csv.writer(mps_csvfile)
-    writer.writerow(["label", "type", "image"])
+    writer.writerow(["label", "type", "description", "image"])
     for mp in mps:
-        favorites = api.GetFavorites(user_id = mp.id)
-        party = "MP"
-        if re.search("labour", mp.description, re.IGNORECASE):
-            party = "Labour"
-        elif re.search("green", mp.description, re.IGNORECASE):
-            party = "Green"
-        elif re.search("national", mp.description, re.IGNORECASE):
-            party = "National"
-        elif re.search("new zealand first|nz first|nz_first", mp.description, re.IGNORECASE):
-            party = "New Zealand First"
-        elif re.search("act", mp.description, re.IGNORECASE):
-            party = "ACT"
-
-        row = mp.name, party, mp.profile_image_url
+        row = mp.name, get_party(mp.description), mp.description, mp.profile_image_url
         print row
         writer.writerow(row)
 
@@ -43,12 +45,6 @@ with open('faves.csv', 'wb') as csvfile:
     writer.writerow(["from", "to"])
 
     for mp in mps:
-        # statuses = api.GetUserTimeline(user_id=mps[0].id, exclude_replies=True)
-        # for status in statuses:
-        #     if status.retweeted_status:
-        #         print mp.name, "RT ", status.retweeted_status.user.name
-        #         writer.writerow([mp.name, status.retweeted_status.user.name])
-
         favorites = api.GetFavorites(user_id = mp.id)
         for favorite in favorites:
             print mp.name, "❤️ ", favorite.user.name
